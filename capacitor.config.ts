de82a0f7991d23c1c5ab7f2af35f6a5203b150a3
unknown <M_INF_5F@isii.pc>
2024-01-18 12:16:02 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'App_monitoraggio',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
